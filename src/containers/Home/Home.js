3cb42b755f0a021d7de3263ed0028a210193a36b
WaylonM <weilong.mao@edu.sait.ca>
2023-01-27 19:57:21 -0700

    Initial commit

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import style from './style.css';
+
+class Home extends Component {
+  render() {
+    return <h1>Weilong's Blog</h1>;
+  }
+}
+
+export default Home;
